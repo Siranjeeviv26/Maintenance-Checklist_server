f751bf6476ff65fce2aca504e538ad6df6e2e4fd
Siranjeevi <101230800+Siranjeeviv26@users.noreply.github.com>
2026-05-12 12:01:18 +0530

    Refactor server.js to use Express and middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,28 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const routes = require("./routes");
 const env = require("./config/env");
+const errorHandler = require("./middlewares/errorHandler");
+const setupSwagger = require("./config/swagger");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: env.clientUrl,
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+setupSwagger(app);
+app.use("/api", routes);
+
+app.use(errorHandler);
 
 app.listen(env.port, () => {
   // eslint-disable-next-line no-console
